3a519df8a05efad29984d3a34190989f32a3a78f
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-12-19 15:14:32 +0100

    Add files via upload

--- app/immutable/chunks/2-7dcaf21a.js
+++ app/immutable/chunks/2-7dcaf21a.js
@@ -1,1 +1,1 @@
-import{default as t}from"../components/pages/_page.svelte-39667b59.js";export{t as component};
+import{default as t}from"../components/pages/page.svelte-39667b59.js";export{t as component};
---
+++ app/immutable/modules/pages/layout.ts-e4e26074.js
@@ -0,0 +1,1 @@
+import{s as o}from"../../chunks/layout-e4a84b88.js";export{o as ssr};
